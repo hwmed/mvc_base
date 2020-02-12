d81634d476c43b2276fcc1c68aec8a0a8f9f97bc
hwmed.sz <badmansog@gmail.com>
2020-02-12 14:17:40 -0800

    update config file

--- configs/config.php
+++ configs/config.php
@@ -4,7 +4,11 @@ $config = array(
     "base_url" => "http://locahost/mvc_base",
     "def_class" => "Main",
     "def_error_class" => "Error",
-    "root" => str_replace("index.php", "" , $_SERVER['SCRIPT_FILENAME'])
+    "root" => str_replace("index.php", "" , $_SERVER['SCRIPT_FILENAME']),
+    "db_host" => "localhost",
+    "db_name" => "shop",
+    "db_user" => "root",
+    "db_pass" => ""
 );
 
 define('BASE_URL', $config['base_url']);
