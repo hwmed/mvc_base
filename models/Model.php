58ca210569563287df0e14266306c5a67137d1cc
hwmed.sz <badmansog@gmail.com>
2020-02-13 09:46:25 -0800

    update model file

--- models/Model.php
+++ models/Model.php
@@ -2,7 +2,33 @@
 
 namespace models;
 
-class Model extends \bases\BaseModel
+abstract class Model extends \bases\BaseModel
 {
-    
+    public function query($query)
+    { 
+        try
+        {
+            $result = $this->pdo->prepare($query);
+
+            
+            $result->execute();
+
+            $rows = array();
+            while($row = $result->fetchObject())
+            {
+                $rows[] = $row;
+            }
+            return $rows;
+        }
+        catch(\PDOException $e)
+        {
+            $this->showErr($e);
+        }
+    }
+
+    abstract public function insert();
+    abstract public function select();
+    abstract public function rowExist();
+    abstract public function update();
+    abstract public function delete();
 }
